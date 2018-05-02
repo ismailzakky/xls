bfbe9af53c85bd3ede16860ed4d6bab26df675fb
Ismail Zakky <ismailzakky@yahoo.com>
2018-05-02 19:17:59 +0700

    remove log

--- format.go
+++ format.go
@@ -17,6 +17,5 @@ type format struct {
 func (f *format) Format(val float64, date1904 bool) string {
 	_, tokens := formatter.Lexer(f.str)
 	ds := formatter.Parse(tokens)
-	fmt.Println("=>", val)
 	return ds.Format(val, date1904)
 }
